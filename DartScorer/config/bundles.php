095caadf54905ad1814f8910c083a7e51afaf144
Lukas Völler <voellerl@MBP-von-Lukas-2.fritz.box>
2024-05-27 13:44:06 +0200

    [TASK] Integrate Symfony Mercure

--- DartScorer/config/bundles.php
+++ DartScorer/config/bundles.php
@@ -14,6 +14,6 @@ return [
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
+    Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
 ];
